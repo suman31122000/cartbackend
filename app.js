eb9d6309b67ee5c0f6fd3b722037f0ad8d9c1570
suman bando <sumanbando31122000@gmail.com>
2024-11-20 18:58:32 +0530

    update

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ import cookieParser from "cookie-parser";
 
 const app=express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://wcommerce.netlify.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.get("/", (req, res) => {
