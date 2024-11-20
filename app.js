560cfbc6b5c7f108c1be03b7d60922f9a9c5961b
suman bando <sumanbando31122000@gmail.com>
2024-11-20 17:18:21 +0530

    update

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.get("/", (req, res) => {
-  res.send("Hello from userRouter!");
-  });
-app.use("/v1",userRouter);
+// app.get("/", (req, res) => {
+//   res.send("Hello from userRouter!");
+//   });
+app.use("/",userRouter);
 
 export default app;
