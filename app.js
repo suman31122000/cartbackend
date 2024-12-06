afd42de386bf9f4ef59f1c33483065e753737205
suman bando <sumanbando31122000@gmail.com>
2024-12-07 00:03:10 +0530

    payment integrated

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ import userRouter from "./router/user.route.js";
 import cookieParser from "cookie-parser";
 
 const app=express();
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 
 app.use(cors({
   origin: 'https://wcommerce.netlify.app',
